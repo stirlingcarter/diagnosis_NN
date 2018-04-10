dfe0a1f2fdb3c08ea511dba1f487c97063e2082c
Stirling Carter <stirling.h.carter@vanderbilt.edu>
2018-04-10 12:19:10 +0800

    init

---
+++ engine.py
@@ -0,0 +1,11 @@
+import tens
+
+#input function returns tuple of input parameters and respective classification
+def input_evaluation_set():
+    features = {'SepalLength': np.array([6.4, 5.0]),
+                'SepalWidth':  np.array([2.8, 2.3]),
+                'PetalLength': np.array([5.6, 3.3]),
+                'PetalWidth':  np.array([2.2, 1.0])}
+    labels = np.array([2, 1])
+    return features, labels
+
